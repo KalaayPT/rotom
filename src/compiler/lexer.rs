10f8500d4a2eaaaeb43110eed93734ed2d10334c
KalaayPT <contact@kalaay.dev>
2026-02-10 20:37:02 +0100

    add item workaround while upstream isnt fixed

--- src/compiler/lexer.rs
+++ src/compiler/lexer.rs
@@ -248,7 +248,7 @@ fn is_identifier_start(c: char) -> bool {
     c.is_alphabetic() || c == '_'
 }
 fn is_identifier_char(c: char) -> bool {
-    c.is_alphanumeric() || c == '_'
+    c.is_alphanumeric() || c == '_' || c == '’' // TODO: potentially hacky, this is to support "ITEM_OAK’S_LETTER", already fixed in dspre canary
 }
 
 impl<'a> Iterator for Lexer<'a> {
